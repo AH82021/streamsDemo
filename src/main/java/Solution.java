f88688ac32ee0c8fd149efcbc603410387587db9
Ajmal Hakim <samimajmal@yahoo.com>
2023-10-10 12:37:17 -0400

    add names method

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,6 +3,7 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(malesOnly(Person.persons()));
+        names(Person.persons()).forEach(System.out::println);
     }
    // Filter the list of persons to include only males.
     static List<String> malesOnly(List<Person> people){
@@ -13,6 +14,14 @@ public class Solution {
                 .toList();
         return males;
 
+    }
+    //Map the list of persons to their names.
+
+    static List<String> names(List<Person> people){
+        List<String> names = people.stream()
+                .map(Person::getName)
+                .toList();
+        return names;
     }
 
 }
